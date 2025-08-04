b993590fed9d7abfc6e47d0f3083dae2db7379d5
Cameron Phillips <cphillips124@gmail.com>
2025-08-04 18:12:19 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 import socket  # noqa: F401
 import struct
+from threading import Thread
 from typing import Tuple
 
 TAG_BUFFER = b'\x00'
-
 
 def construct_header_line(data: bytes) -> bytes:
     try:
@@ -25,17 +25,30 @@ def construct_header_line(data: bytes) -> bytes:
     message_leng = bytearray(len(message).to_bytes(4, byteorder='big'))
     return message_leng + message
 
-
-def main():
-    print("Logs from your program will appear here!")
-    server = socket.create_server(("localhost", 9092))
-    sock, addr = server.accept() # wait for client
+def handle(sock: socket.socket):
     while True:
         data = sock.recv(1024)
         if not data:
             break
+        print(data)
         header = construct_header_line(data[:12])
         sock.sendall(header)
+
+
+
+def main():
+    print("Logs from your program will appear here!")
+    server = socket.create_server(("localhost", 9092))
+    try:
+        while True:
+            sock, addr = server.accept() #  wait for client
+            thread = Thread(target=handle, args=(sock, ))
+            thread.start()
+    except KeyboardInterrupt:
+        print('Interrupted by Keyboard')
+        pass
+    finally:
+        pass
 
 
 if __name__ == "__main__":
