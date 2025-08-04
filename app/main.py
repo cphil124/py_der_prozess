487f6befaf751740613add926c32682639d7ef0e
Cameron Phillips <cphillips124@gmail.com>
2025-08-04 12:42:15 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 import socket  # noqa: F401
 import struct
+from typing import Tuple
 
 BYTE = bytes(1)
 
@@ -10,6 +11,7 @@ def construct_header_line(message_size:int, corr_id: int):
     # return [message_size, correlation_id_bytes]
     return struct.pack('>ii', message_size, corr_id)
 
+
 def main():
     # You can use print statements as follows for debugging,
     # they'll be visible when running tests.
@@ -19,13 +21,19 @@ def main():
     #
     server = socket.create_server(("localhost", 9092))
     sock, addr = server.accept() # wait for client
+    data = sock.recv(1024)
+    message_size, api_key, api_version, correlation_id = struct.unpack('>ihhI', data[:12])
+    print(message_size, api_key, api_version, correlation_id)
+    # print(type(data))
+    # print(data)
+    # print("raw_data:",data)
+    # message_size = int.from_bytes(data[:4], byteorder='big')
+    # api_key = int.from_bytes(data[4:6])
+    # api_version = int.from_bytes(data[6:8])
+    # correlation_id = int.from_bytes(data[8:12])
+    # client_id = data[12:].decode('ascii')
     
-    chunk = sock.recv(2048)
-    print(type(chunk))
-    decoded = chunk.decode()
-    print(type(decoded))
-    print(decoded)
-    header = construct_header_line(4, 7)
+    header = construct_header_line(4, correlation_id)
     sock.sendall(header)
 
 
