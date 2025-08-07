88514fc703413c41243ace8dc60422f8cdac68f8
Cameron Phillips <cphillips124@gmail.com>
2025-08-07 09:59:06 -0500

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,17 +1,96 @@
 import socket  # noqa: F401
 import struct
+import codecs
 from abc import ABC
 from threading import Thread
-from typing import Tuple
+from typing import Tuple, Callable
+from collections import namedtuple
 # from constants import *
 
 TAG_BUFFER = b'\x00'
+NULL_TOPIC_ID = bytes(16)
 
-class KafkaRequest:
+topics = {'NULL': NULL_TOPIC_ID}
+
+"""
+    API Version Request Header:
+        - API Key
+        - API Version
+        - Correlation ID
+        - Client ID
+        - Tag Buffer
+    API Version Request Header:
+        - Correlation ID
+        - Tag Buffer (Only v1, v0 has no buffer)
+
+    DescribePartition Request Header:
+        - API Key
+        - API Version
+        - Correlation ID
+        - Client ID
+        - Tag Buffer
+    DescribePartition Response Header:
+        - Correlation ID
+        - Tag Buffer
+"""
+
+def response_wrapper(func: Callable, *args, **kwargs) -> Callable:
+    def wrapper():
+        message = func(args[0])
+        message_len = len(message).to_bytes(4)
+        return message_len + message + TAG_BUFFER
+    return wrapper
+
+class KafkaRequest(ABC):
     def __init__(self, req_bytes: bytes):
         self.message_size, self.api_key, self.api_version, self.correlation_id = struct.unpack('>ihhI', req_bytes[0:12])
-        print('Message Received', self.message_size, self.api_key, self.api_version, self.correlation_id)
+        # print('Message Received', self.message_size, self.api_key, self.api_version, self.correlation_id)
+        self.client_name = ''
         self.error_code = 0
+
+class KafkaApiVersionRequest(KafkaRequest):
+    pass
+
+class KafkaDescribePartitionsRequest(KafkaRequest):
+    def __init__(self, req_bytes: bytes):
+        cur_beg = 0
+        cur_end = 12
+        super().__init__(req_bytes=req_bytes[cur_beg:cur_end])
+        self.request_topics = []
+
+        cur_beg = cur_end
+        cur_end += 2
+        content_length = int(struct.unpack('>h', req_bytes[cur_beg:cur_end])[0])
+        
+        # print('content length', content_length)
+        cur_beg += 2
+        cur_end = cur_beg + content_length + 1
+        self.client_name = struct.unpack(f'>{content_length}sx', req_bytes[cur_beg:cur_end])[0].decode()
+        # print(self.client_name)
+
+        cur_beg = cur_end
+        cur_end += 1
+        # print(cur_beg, cur_end)
+        array_length = struct.unpack('>b', req_bytes[cur_beg: cur_end])[0]
+        # print(array_length)
+
+        for i in range(array_length-1):
+            cur_beg = cur_end
+            cur_end += 1
+            topic_name_leng = int(struct.unpack('>b', req_bytes[cur_beg:cur_end])[0])
+            # print('topic_name_leng', topic_name_leng)
+            cur_beg += 1
+            cur_end += topic_name_leng
+            # print(cur_beg, cur_end)
+            topic_name = struct.unpack(f'>{topic_name_leng-1}sx', req_bytes[cur_beg:cur_end])[0].decode()
+            print(topic_name)
+            # print('Topic Name:', topic_name)
+            self.request_topics.append(topic_name)
+        cur_beg = cur_end
+        cur_end += 6
+        # print(cur_beg, cur_end, len(req_bytes[cur_beg:]), req_bytes[cur_beg:])
+        response_limit, page_cursor = struct.unpack('>ibx', req_bytes[cur_beg:cur_end])
+        # print(self.request_topics)
 
 class KafkaResponse(ABC):
     API_KEY = -1
@@ -20,6 +99,7 @@ class KafkaResponse(ABC):
     def __init__(self, req: KafkaRequest):
         self.correlation_id = req.correlation_id
         self.api_version = req.api_version
+        self.client_name = req.client_name
         self.error_code = 0
 
     @classmethod
@@ -27,9 +107,8 @@ class KafkaResponse(ABC):
         ver_arr = bytearray(cls.API_KEY.to_bytes(2, byteorder='big'))
         ver_arr += bytearray(cls.MIN_VERSION.to_bytes(2, byteorder='big'))
         ver_arr += bytearray(cls.MAX_VERSION.to_bytes(2, byteorder='big'))
-        print(ver_arr)
         return ver_arr
-
+    
     def construct_response_message(self) -> bytes:
         return b''
 
@@ -38,42 +117,97 @@ class KafkaAPIVersionsResponse(KafkaResponse):
     MIN_VERSION = 0
     MAX_VERSION = 4
 
+    # @response_wrapper
     def construct_response_message(self) -> bytes:
         message = bytearray(self.correlation_id.to_bytes(4, byteorder='big'))
         message += self.error_code.to_bytes(2, byteorder='big')
-        message += int(len(RESPONSE_TYPES)+1).to_bytes(1, byteorder='big') # Array length is n + 1
-        for _, typ in RESPONSE_TYPES.items():
-            message += typ.get_api_versions()
+        message += int(len(API_TYPES)+1).to_bytes(1, byteorder='big') # Array length is n + 1
+        for _, typ in API_TYPES.items():
+            message += typ.response.get_api_versions()
             message += TAG_BUFFER
         message += int(0).to_bytes(4, byteorder='big') # Throttle Time ms
         message += TAG_BUFFER
         message_leng = bytearray(len(message).to_bytes(4, byteorder='big'))
         ret_message = message_leng + message
-        print(ret_message)
         return ret_message
 
 class KafkaDescribeTopicPartitionsResponse(KafkaResponse):
     API_KEY = 75
     MIN_VERSION = 0
     MAX_VERSION = 0
+    
+
+    def __init__(self, req: KafkaDescribePartitionsRequest):
+        super().__init__(req)
+        self.requested_topics = req.request_topics
+        self.topic_errors = {topic: (0 if topic in topics else 3)  for topic in req.request_topics}
+            
 
 
-RESPONSE_TYPES = {
-    18: KafkaAPIVersionsResponse,
-    75: KafkaDescribeTopicPartitionsResponse
+    # @response_wrapper
+    def construct_response_message(self) -> bytes:
+        response_limit = 100
+        message = bytearray(self.correlation_id.to_bytes(4, byteorder='big'))
+        message += TAG_BUFFER
+        message += bytes(4) # Throttle Time
+        message += int(len(self.requested_topics)+1).to_bytes(1, byteorder='big')
+        for topic_name, error_code in self.topic_errors.items():
+            topic_description = bytearray(error_code.to_bytes(2, byteorder='big'))
+            topic_description += int(len(topic_name)+1).to_bytes(1, byteorder='big')
+            print(topic_name)
+            topic_description += topic_name.encode('utf-8')
+            topic_id = topics[topic_name] if topics.get(topic_name) else NULL_TOPIC_ID
+            topic_description += topic_id
+            topic_description += TAG_BUFFER
+            partitions_array_length = 1
+            topic_description += partitions_array_length.to_bytes(1, byteorder='big')
+            topic_description += bytes(4)
+            topic_description += TAG_BUFFER
+            message += topic_description
+        message += int(255).to_bytes(1, byteorder='big')
+        message += TAG_BUFFER
+        message_leng = bytearray(len(message).to_bytes(4, byteorder='big'))
+        ret_message = message_leng + message 
+        print(ret_message)
+        return ret_message
+
+
+
+#TODO: Refactor to have a catalog of some sort. Extract the version min/max from the response, and decouple the request from the response.
+class ApiType:
+    def __init__(self, apikey:int, req: type[KafkaRequest], resp: type[KafkaResponse], min_version: int, max_version: int):
+        self.api_key = apikey
+        self.request = req
+        self.response = resp
+        self.min_version = min_version
+        self.max_version = max_version
+
+
+API_TYPES = {
+    18: ApiType(18, KafkaApiVersionRequest, KafkaAPIVersionsResponse, 0, 4),
+    75: ApiType(75, KafkaDescribePartitionsRequest, KafkaDescribeTopicPartitionsResponse, 0, 0)
 }
 
+#TODO: Decouple from KafkaRequest
 def create_response(req: KafkaRequest) -> KafkaResponse:
     error_code = 0
-    if response_type := RESPONSE_TYPES[req.api_key]:
-        response = response_type(req)
-        print(f'Api Type: {type(response)}; Request API Version: {req.api_version}; API Min Version: {response.MIN_VERSION}; API Max Version: {response.MAX_VERSION};')
-        if not response_type.MIN_VERSION <= req.api_version <= response_type.MAX_VERSION:
+    if api_type := API_TYPES[req.api_key]:
+        response = api_type.response(req)
+        # print(f'Api Type: {type(response)}; Request API Version: {req.api_version}; API Min Version: {response.MIN_VERSION}; API Max Version: {response.MAX_VERSION};')
+        if not api_type.response.MIN_VERSION <= req.api_version <= api_type.response.MAX_VERSION:
             response.error_code = 35
-        print(f'Error Code: {response.error_code}')
         return response
     else:
         return KafkaResponse(req)
+    
+
+def request_parser(data:bytes) -> KafkaRequest:
+    api_version = int(struct.unpack('>h', data[4:6])[0])
+    print(api_version)
+    if api_type := API_TYPES.get(api_version):
+        return api_type.request(data)
+    else:
+        return KafkaRequest(TAG_BUFFER)
 
 
 def handle(sock: socket.socket):
@@ -81,14 +215,10 @@ def handle(sock: socket.socket):
         data = sock.recv(1024)
         if not data:
             break
-        print(data)
-        request = KafkaRequest(data)
+        request = request_parser(data)
         response = create_response(request)
-        if response.API_KEY == -1:
-            continue
         response_message = response.construct_response_message()
         sock.sendall(response_message)
-
 
 
 def main():
